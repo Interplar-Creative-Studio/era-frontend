a9e5a5ac9e20f291ef6de3b9ec44fb20da7d6cdb
SyperOlao <olay1234538@gmail.com>
2021-11-18 23:16:29 +0300

    Добавлены компоненты Message.js и Notification.js в  HeaderMainMenu.js. Теги <a> заменены на NavLink

--- src/MainAppication/Header/HeaderMainMenu/HeaderMainMenu.js
+++ src/MainAppication/Header/HeaderMainMenu/HeaderMainMenu.js
@@ -1,17 +1,21 @@
 import React from "react";
 import {Search} from "./Search/Search";
+import {NavLink} from "react-router-dom";
+import {Notification} from "./Notification/Notification";
+import {Message} from "./Message/Message";
 
 export const HeaderMainMenu = () => {
-    //TODO:: выяснить куда ведет тег <a>
     return(
         <div className="menu__first-block">
-            <a className="menu__first-block__logo">
+            <NavLink to="#" className="menu__first-block__logo">
                 <p>Era</p>
-            </a>
+            </NavLink>
             <div className="menu__first-block__description">
                 <p>Лента</p>
             </div>
             <Search/>
+            <Notification/>
+            <Message/>
         </div>
     );
 };
--- src/MainAppication/Header/HeaderMainMenu/Search/Search.js
+++ src/MainAppication/Header/HeaderMainMenu/Search/Search.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {Tags} from "../Tags/Tags";
+import {Tags} from "../../../Components/Tags/Tags";
 
 export const Search = (props) => {
     let titlesAndTags = {
-        ['Недавно искали']: {tags: ['Wallpapers', 'Wallpapers', 'Wallpapers']},
-        ['Популярные запросы']: {tags: ['Wallpapers', 'Wallpapers', 'Wallpapers']},
-        ['Популярные темы']: {tags: ['Wallpapers', 'Wallpapers', 'Wallpapers', 'Wallpapers']},
-        ['Популярные коллекции']: {tags: ['Wallpapers', 'Wallpapers', 'Wallpapers']},
+        "Недавно искали": {tags: ['Wallpapers', 'Wallpapers', 'Wallpapers']},
+        "Популярные запросы": {tags: ['Wallpapers', 'Wallpapers', 'Wallpapers']},
+        "Популярные темы": {tags: ['Wallpapers', 'Wallpapers', 'Wallpapers', 'Wallpapers']},
+        "Популярные коллекции": {tags: ['Wallpapers', 'Wallpapers', 'Wallpapers']},
     };
 
     return (
