29a74232f352e2d8f360d0fc6b9631af953db02e
SyperOlao <olay1234538@gmail.com>
2021-11-20 10:14:46 +0300

    Создание новой компоненты Card

--- src/MainAppication/Container/Container.js
+++ src/MainAppication/Container/Container.js
@@ -4,19 +4,20 @@ import {PicturesMainPage} from "./Pages/PicturesMainPage/PicturesMainPage";
 import {Redirect, Switch} from "react-router";
 import {PersonalPage} from "./Pages/PersonalPage/PersonalPage";
 import {PersonalSettingsPage} from "./Pages/PersonalSettingsPage/PersonalSettingsPage";
-import {PAGE, PERSONAL, PERSONAL_SETTINGS} from "../../UrlsConst";
+import {CART, PAGE, PERSONAL, PERSONAL_SETTINGS} from "../../UrlsConst";
+import {Cart} from "./Pages/Cart/Cart";
 
 
 export const Container = (props) => {
     return (
         <Switch>
             <Route exact path="/">
-                <Redirect to={PERSONAL_SETTINGS}/>
+                <Redirect to={CART}/>
             </Route>
             <Route path={PAGE} render={() => <PicturesMainPage/>}/>
             <Route path={PERSONAL} render={() => <PersonalPage/>}/>
             <Route path={PERSONAL_SETTINGS} render={() => <PersonalSettingsPage/>}/>
-
+            <Route path={CART} render={()=><Cart/>}/>
         </Switch>
 
     );
