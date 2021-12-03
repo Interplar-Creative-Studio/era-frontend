f9a0a4b7ae420dd9f577e034cfe61a40f4b52245
SyperOlao <olay1234538@gmail.com>
2021-12-03 12:34:41 +0300

    Бесконечный скорлл 2

--- src/MainAppication/Container/Pages/PicturesMainPage/PicturesMainPage.js
+++ src/MainAppication/Container/Pages/PicturesMainPage/PicturesMainPage.js
@@ -7,24 +7,49 @@ import {LoginMenu} from "../../../Header/HeaderMainMenu/LoginMenu/LoginMenu";
 
 
 function getPage(num, index = 0) {
-    if (index >= num ) return 1
+    if (index >= num) return 1
     if (index < 0) return num
     return index
 }
+
 
 const PicturesMainPage = (props) => {
     const [page, setPage] = useState(1);
     const [pictures, setPictures] = useState([]);
     const [showPictures, setShowPictures] = useState(false);
-    function getArr(){
+
+    function getArr() {
         url = `${process.env.REACT_APP_API_URL}/api/photostock/?page=${page}&tag_id=${props.tagId ?? 1}`;
-        fetch(url).then(response => response.json().then(res => setPictures( pictures.concat(res))));
-        setPage(prev=>getPage(3,prev+1));
+        fetch(url).then(response => {
+            if (response.ok) {
+                response.json().then(res => setPictures(pictures.concat(res)))
+            }
+        });
+        setPage(prev => getPage(0, 0));
     }
+
     let url = `${process.env.REACT_APP_API_URL}/api/photostock/?page=${page}&tag_id=${props.tagId ?? 1}`;
+    const [currTag, setCurrTag] = useState();
+
+    function check(tag, curTag) {
+        if (tag !== curTag) {
+            setPictures([]);
+            setPage(0);
+            setCurrTag(tag);
+        }
+    }
+
+    useEffect(()=>{
+        check(props.tagId, currTag);
+    }, [page, props.tagId]);
+
     useEffect(() => {
-        getArr()
+        //check(props.tagId, currTag);
+        getArr();
     }, [props.tagId]);
+
+    console.log(pictures);
+    console.log("page:", page);
 
     const onClick = () => {
         setShowPictures(!showPictures);
@@ -34,11 +59,10 @@ const PicturesMainPage = (props) => {
         setTimeout(() => setTime(true), 5000);
     }
 
-    const getModal = () =>{
+    const getModal = () => {
         getTime();
         return isTime && <LoginMenu/>
     }
-
 
 
     return (
@@ -53,7 +77,6 @@ const PicturesMainPage = (props) => {
         </>
     );
 };
-
 
 
 const mapStateToProps = (state) => {
