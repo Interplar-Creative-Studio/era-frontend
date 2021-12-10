2734a73f7502d7c33781e8e13893ba000b7f5d12
SyperOlao <olay1234538@gmail.com>
2021-12-10 03:33:19 +0300

    изменение в ксс

--- src/MainAppication/Header/HeaderMainMenu/HeaderUser/HeaderUser.js
+++ src/MainAppication/Header/HeaderMainMenu/HeaderUser/HeaderUser.js
@@ -4,16 +4,13 @@ import {User} from "../../../Components/User/User";
 
 export const HeaderUser = (props) => {
     const [exitMenu, setExitMenu] = useState(false);
-/*    const onMouseOverMenu = () => {
-        setExitMenu(true);
-    }*/
+
     const onClick= () =>{
         setExitMenu(!exitMenu);
     }
     return(
         <div onClick={onClick}>
-        <div className="menu__first-block__burger__block__user-profile menu__first-block__user-profile"
-             >
+        <div className="menu__first-block__burger__block__user-profile menu__first-block__user-profile">
             <User imgLink={"#"} img={props.img} nameLink={"#"} name={props.name}/>
             {exitMenu && <ExitPanel logOut={props.logOut}/>}
         </div>
